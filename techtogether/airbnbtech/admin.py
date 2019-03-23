c7ac113add4a9d6980e84b337d2c837804464d79
CCalderon01 <caroelizcalderon@gmail.com>
2019-03-22 22:16:51 -0500

    added admin interface

--- techtogether/airbnbtech/admin.py
+++ techtogether/airbnbtech/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import FinalLocation, Location, Transportation, Food
+
+@admin.register(FinalLocation)
+class FinalAdmin(admin.ModelAdmin):
+    list_display = ['url', 'latitude', 'longitude', 'zip_code', 'id',
+                    'name', 'neighborhood', 'accomodates', 'room_type',
+                    'price', 'review_rating', 'crime_rate', 'food_rate',
+                    'transport_rate']
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['url', 'latitude', 'longitude', 'zip_code', 'id',
+                    'name', 'neighborhood', 'accomodates', 'room_type',
+                    'price', 'review_rating']
+
+@admin.register(Transportation)
+class TransportationAdmin(admin.ModelAdmin):
+    list_display = ['latitude', 'longitude', 'name']
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ['zip_code', 'name']
